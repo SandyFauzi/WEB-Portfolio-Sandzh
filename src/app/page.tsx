45b440bfc6c1abe611485538293d12a40adc9dfe
Sandy Fauzi <sandyfauzia09@gmail.com>
2026-03-27 15:26:28 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default async function HomePage() {
         <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4 md:px-12">
           
           {/* Logo (Otomatis Invert Warna di CSS) */}
-          <div className="h-6 w-28 md:h-7 md:w-32 opacity-90 transition-opacity hover:opacity-100">
+          <div className="h-9 w-31 md:h-15 md:w-38 opacity-90 transition-opacity hover:opacity-100">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img 
               src="/Sandzh Black.png" 
